2196f7cfdb19996ce14403b8dda18cefcb0fe802
DobrelyaVladimir <vovchick1984@gmail.com>
2022-10-30 14:37:26 +0200

    breeze install

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/', [\App\Http\Controllers\ProductController::class, 'index'])->name('index');
-Route::get('/products/{product}', [\App\Http\Controllers\ProductController::class, 'show'])->name('product');
-Route::get('/brands', [\App\Http\Controllers\BrandController::class, 'index'])->name('brands');
-Route::get('/brands/{brand}', [\App\Http\Controllers\BrandController::class, 'brand'])->name('brand');
-Route::get('/basket',[\App\Http\Controllers\BasketController::class, 'basket'])->name('basket');
-Route::get('/basket/place',[\App\Http\Controllers\BasketController::class, 'basketPlace'])->name('basket-place');
-Route::post('/basket/add/{id}', [\App\Http\Controllers\BasketController::class, 'basketAdd'])->name('basket-add');
-Route::post('/basket/remove/{id}', [\App\Http\Controllers\BasketController::class, 'basketRemove'])->name('basket-remove');
-Route::post('/basket/place',[\App\Http\Controllers\BasketController::class, 'basketConfirm'])->name('basket-confirm');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
